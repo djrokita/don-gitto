9603aead0cebca817648a813cccdd5e6b8638d7a
Krzych Pierzchala <krzysztof.pierzchala@likims.com>
2020-02-11 11:04:24 +0100

    fix styles

--- src/components/box/Box.styles.js
+++ src/components/box/Box.styles.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const BoxContainer = styled.main`
-  width: 1200px;
+  width: 70%;
+  min-width: 1200px;
   outline: 1px solid red;
   display: flex;
 `;
--- src/components/content/Content.styles.js
+++ src/components/content/Content.styles.js
@@ -3,4 +3,6 @@ import styled from "styled-components";
 export const ContentContainer = styled.main`
   display: flex;
   flex: 1 0 auto;
+  width: 100%;
+  justify-content: center;
 `;
